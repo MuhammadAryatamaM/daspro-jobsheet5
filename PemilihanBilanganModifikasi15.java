dd3c165316984441749905f16d32302f0d04f8cd
MuhammadAryatamaM <muhammad.aryatama2006@gmail.com>
2024-09-26 07:38:15 +0700

    program percobaan 1 if else

---
+++ PemilihanBilanganModifikasi15.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class PemilihanBilanganModifikasi15 {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+
+        String hasil = ((angka % 2) == 0) ? ("termasuk bilangan genap") : ("termasuk bilangan ganjil");
+        System.out.println("Angka " + angka + " " + hasil);
+    }
+}
